d45efbb1b4880d074faf251cf26f2a5d7155609e
Naga <Naga@gmail.com>
2026-01-13 19:42:50 +0400

    Refactor File Name

--- database/DBConnection.java
+++ database/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 public class DBConnection {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/bachandb";
-        String username = "root";
-        String password = "Pushkaar21$";
+        String username = "";
+        String password = "";
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
             System.out.println("Database connected sucessfully!");
