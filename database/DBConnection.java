e740b1a0a659346134224399c4ca4e5518298be7
Tishar <mbeerbul@gmail.com>
2026-01-20 18:48:34 +0400

    hopefully fix

--- database/DBConnection.java
+++ database/DBConnection.java
@@ -1,19 +1,19 @@
 package database;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/bachandb";
-        String username = "root";
-        String password = "Pushkaar21$";
 
-        try (Connection conn = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected sucessfully!");
-        } catch (SQLException e) {
-            System.out.println("Database connection failed");
-            e.printStackTrace();
-        }
+    private static final String URL = "jdbc:mysql://localhost:3306/bachandb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
+
 }
+
