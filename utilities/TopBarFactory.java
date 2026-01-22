1bbc41e5090e10d265c78b592119c98cad886624
naga1234 <nagapushkaar@gmail.com>
2026-01-22 23:24:17 +0400

    TopBarFactory 2.0 with the buttons

--- utilities/TopBarFactory.java
+++ utilities/TopBarFactory.java
@@ -3,18 +3,65 @@ package utilities;
 import javax.swing.*;
 import java.awt.*;
 
-public class TopBarFactory {
+public class TopBarFactory extends JPanel {
 
-    private TopBarFactory() {}
+    public final JButton btnAdd;
+    public final JButton btnClear;
+    public final JButton btnUpdate;
+    public final JButton btnDelete;
+    public final JButton btnSearch;
+    public final JComboBox<String> cmbSort;
+    public final JTextField txtSearch;
 
-    public static JPanel createTopBar(JComponent... components) {
-        JPanel topBar = new JPanel(new FlowLayout(FlowLayout.RIGHT, 8, 5));
-        topBar.setBackground(new Color(240, 240, 240));
+    public TopBarFactory(String searchLabelText, String[] sortOptions) {
 
-        for (JComponent c : components) {
-            topBar.add(c);
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 8, 5));
+        setBackground(new Color(240, 240, 240));
+
+        Font fontBold = new Font("Segoe UI", Font.BOLD, 14);
+        Color primaryBlue = new Color(30, 75, 176);
+
+        // Buttons
+        btnAdd    = ButtonFactory.createButtonPlain();
+        btnClear  = ButtonFactory.createButtonPlain();
+        btnUpdate = ButtonFactory.createButtonPlain();
+        btnDelete = ButtonFactory.createButtonPlain();
+        btnSearch = ButtonFactory.createButtonPlain();
+
+        btnAdd.setText("Add");
+        btnClear.setText("Clear");
+        btnUpdate.setText("Update");
+        btnDelete.setText("Delete");
+        btnSearch.setText("Search");
+
+        JButton[] buttons = {
+                btnAdd, btnClear, btnUpdate, btnDelete, btnSearch
+        };
+
+        for (JButton b : buttons) {
+            b.setBackground(primaryBlue);
+            b.setForeground(Color.WHITE);
+            b.setFont(fontBold);
         }
 
-        return topBar;
+        // Sort combo
+        cmbSort = ComboBoxFactory.createFormComboBox(sortOptions);
+
+        // Search
+        JLabel lblSearch = new JLabel(searchLabelText);
+        lblSearch.setFont(fontBold);
+
+        txtSearch = new JTextField(40);
+        txtSearch.setFont(fontBold);
+
+        // Add components
+        add(btnAdd);
+        add(btnClear);
+        add(btnUpdate);
+        add(btnDelete);
+        add(cmbSort);
+        add(lblSearch);
+        add(txtSearch);
+        add(btnSearch);
     }
 }
